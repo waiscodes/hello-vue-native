7e359468ec2fcebc2fba67a990f656edb27ea6a5
waiscodes <b.wais@outlook.com>
2020-12-31 14:49:49 -0700

    changed hello world message and got it to show up on my phone

--- App.vue
+++ App.vue
@@ -1,6 +1,6 @@
 <template>
   <view class="container">
-    <text class="text-color-primary">My Vue Native App</text>
+    <text class="text-color-primary">You alright mate?</text>
   </view>
 </template>
 
