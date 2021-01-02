37d22852f1fa06558b08ce982f883f590942a501
waiscodes <b.wais@outlook.com>
2021-01-01 20:39:39 -0700

    went back to basics to break things and fix them

--- App.vue
+++ App.vue
@@ -1,13 +1,17 @@
 <template>
-  <text> {{ item.id }}. {{ item.text }} </text>
+  <view class="container">
+    <text class="text-color-primary">You alright mate?</text>
+  </view>
 </template>
 
-<script>
-export default {
-  props: {
-    item: {
-      Type: Object,
-    },
-  },
-};
-</script>
+<style>
+.container {
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+.text-color-primary {
+  color: blue;
+}
+</style>
