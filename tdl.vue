b437872cbb2b7888440767a3e2f85a1bbfb4f299
waiscodes <b.wais@outlook.com>
2021-01-01 20:34:29 -0700

    created a vue component and rendered on phone

---
+++ tdl.vue
@@ -0,0 +1,46 @@
+<template>
+  <view class="container">
+    <todo-item
+      class="text-container"
+      v-for="todo in todos"
+      :key="todo.text"
+      :item="todo"
+    />
+  </view>
+</template>
+
+<script>
+import TodoItem from "./todoItem";
+export default {
+  components: { TodoItem },
+  data: function () {
+    return {
+      todos: [
+        { id: 1, text: "Learn JavaScript" },
+        { id: 2, text: "Learn Vue" },
+        { id: 3, text: "Build something awesome" },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+.container {
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+.text-container {
+  color: blue;
+  padding: 2;
+  font-size: 22;
+}
+.text-input-container {
+  width: 300;
+  height: 40;
+  font-size: 22;
+  border-color: gray;
+}
+</style>
