3c20f2c269627b14d9f01a4bc434335e0e074028
waiscodes <b.wais@outlook.com>
2021-01-04 19:55:15 -0700

    added input and button

--- components/Random.vue
+++ components/Random.vue
@@ -1,6 +1,10 @@
 <template>
   <view class="container">
     <text>Hello from Random Compoent</text>
+    <text-input
+      :style="{ height: 40, width: 200, borderColor: 'gray', borderWidth: 1 }"
+    />
+    <button title="Add To Do" />
   </view>
 </template>
 
